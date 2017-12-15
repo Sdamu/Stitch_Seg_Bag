6f759693980c32448e6fe7b09139c6739ea2b978
Sdamu <shr1994@yeah.net>
2017-12-15 19:17:45 +0800

    2017.12.15

--- Stitch_Seg_Bag/stitch_seg_bag.cpp
+++ Stitch_Seg_Bag/stitch_seg_bag.cpp
@@ -106,6 +106,17 @@ Mat First_Process(Mat &srcImage)
 	return src_Binary;
 }
 
+// 不去水印，将图像变为二值图像进行返回
+Mat color_2_Binary(Mat srcImage)
+{
+	Mat gray_srcImage;
+	cvtColor(srcImage, gray_srcImage, CV_BGR2GRAY);
+	Mat src_Binary;
+	threshold(gray_srcImage, src_Binary, 200, 255, THRESH_BINARY);
+	return src_Binary;
+}
+
+
 // 输入一张图像，判断第一列与最后一列是否均为白色
 // 若第一列与最后一列均为白色，返回 '11'
 // 若第一列不为白色，最后一列为白色，返回'01'
@@ -137,12 +148,113 @@ string begin_end_white_condition(Mat Binary_srcImage)
 		return "10";
 }
 
-//拼接函数，输入的是待拼接的文件名的容器，输出的是拼接后的图像
-Mat stitch_seg_bag(vector<string> Stitch_images)
+// 两张图像的拼接函数
+Mat Stitch_2_Images(Mat srcImage1, Mat srcImage2)
 {
+	srcImage2 = srcImage2(Rect(1, 0, srcImage2.cols - 1, srcImage2.rows));
+	// 进行灰度化处理
+	Mat grayImage1, grayImage2;
+	cvtColor(srcImage1, grayImage1, CV_BGR2GRAY);
+	cvtColor(srcImage2, grayImage2, CV_BGR2GRAY);
+
+	// 对灰度图像进行特殊的阈值处理
+	// 大于 230，不考虑，找到第一个小于230的像素
+	int pos1 = 0;	// 记住第一张图里最后一列第一个小于230的像素
+	for (int i = 0; i < grayImage1.rows; i++)
+	{
+		if (grayImage1.at<uchar>(i, grayImage1.cols - 1) < 230)
+		{
+			pos1 = i;
+			break;
+		}
+		else
+			continue;
+	}
+	int pos2 = 0;
+	for (int j = 0; j < grayImage2.rows; j++)
+	{
+		if (grayImage2.at<uchar>(j, 0) < 230)
+		{
+			pos2 = j;
+			break;
+		}
+		else
+			continue;
+	}
+
+	// 利用这两个相对位置进行拼接，将这两个位置进行做差计算，求取拼接位置
+	// 用第一张图像的 pos1 减去 第二张图像 的 pos2
+	// 如果这个差值大于0，令第二张图像向下平移相对位移再进行拼接
+	// 如果这个差值小于0，令第二张图像向上平移相对位移再进行拼接
+	int diff = pos1 - pos2;
+	// 首先求出总宽度
+	int result_width = srcImage1.cols + srcImage2.cols;
+	int result_height = 3000;	// 程序预设值
+	Mat result_Image;
+	result_Image.create(Size(result_width, result_height), CV_8UC3);
+	if (diff >= 0)
+	{
+		// 将第一张图像的最左侧中心与result_Image 最左侧中心进行对齐
+		// 然后再进行拼接操作
+		
+		// 确定高度的开始位置
+		int start_row = 1500 - srcImage1.rows / 2;
+		// 首先将第一张图复制到最终图像上
+		for (int i = 0; i < srcImage1.rows; i++)
+		{
+			for (int j = 0; j < srcImage1.cols; j++)
+			{
+				result_Image.at<Vec3b>(start_row + i, j)[0] = srcImage1.at<Vec3b>(i, j)[0];
+				result_Image.at<Vec3b>(start_row + i, j)[1] = srcImage1.at<Vec3b>(i, j)[1];
+				result_Image.at<Vec3b>(start_row + i, j)[2] = srcImage1.at<Vec3b>(i, j)[2];
+			}
+		}
+		// 再讲第二张图像根据相对位移复制到最终图像上
+		for (int i = 0; i < srcImage2.rows; i++)
+		{
+			for (int j = 0; j < srcImage2.cols; j++)
+			{
+
+			}
+		}
+
+	}
+	else
+	{
+
+	}
+
+
 
 }
 
+
+
+
+
+
+
+//拼接函数，输入的是待拼接的文件名的容器，输出的是拼接后的图像
+Mat stitch_seg_bag(vector<string> Stitch_images)
+{
+	Mat result_Mat;
+	int result_width = 0;	// 最后生成图像的总宽度
+	for (size_t i = 0; i < Stitch_images.size(); i++)
+	{
+		Mat Temp_Image = imread(Stitch_images.at(i));
+		result_width += Temp_Image.cols;
+	}
+	// 因为除了第一张图像外，之后的图像第一列均为白色
+	result_width -= (Stitch_images.size() - 1);
+	Mat Temp_Result = imread(Stitch_images[0]);
+	//循环进行拼接操作
+	for (size_t s = 1; s < Stitch_images.size(); s++)
+	{
+		Mat this_Image = imread(Stitch_images[s]);
+		Temp_Result = Stitch_2_Images(Temp_Result, this_Image);
+	}
+	return Temp_Result;
+}
 
 
 
